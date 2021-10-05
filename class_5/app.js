67387f0d42c8a85dd0a54466d05042611830a4c6
ss <ss>
2021-10-05 21:40:16 +0500

    class 5

---
+++ class_5/app.js
@@ -0,0 +1,90 @@
+// let a
+
+// function foo(val) {
+//     a = val ? val : 'write something'
+//     return function baar() {
+//         console.log(a)
+//     }
+// }
+
+
+// function baar() {
+//     let a = 'Return Value'
+//     return a
+// }
+
+
+
+// foo('abc')()
+
+// const b = []
+
+// b[0] = 'text'
+
+
+// let fun = function () {
+//     console.log('hello worlfd')
+// }
+
+// let baar = (val) => {
+//     return val
+// }
+
+// let baar = val => {
+//     return val
+// }
+// let c = 'asdasd'
+// let baar = _ => console.log(_)
+
+// let arr = [ 6, 7, 8, 9]
+// let fil = arr.filter(x => x < 5)
+// fil = fil.length > 0 ? fil : "no value found"
+
+// console.log(fil)
+
+
+
+// baar('without curly Bracket')
+
+
+
+// fun()
+
+
+// console.log(1)
+// setTimeout(()=>console.log(2),1000) 
+// console.log(3)
+// console.log(4)
+
+
+//     let db = new Promise((resolve,reject)=>{
+//         if(user == true){
+//             resolve(data)
+//         }else{
+//             reject(error)
+//         }
+//     })
+
+// db.then().catch()
+
+// async function foo(){
+//     let a = await data
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
