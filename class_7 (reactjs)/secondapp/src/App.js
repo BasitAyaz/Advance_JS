87d11b8c390a03890fbbd29778b91b1dcf8dc628
ss <fff>
2021-10-31 09:44:41 -0700

    complete

--- class_7 (reactjs)/secondapp/src/App.js
+++ class_7 (reactjs)/secondapp/src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <div className="App"> 
       <header className="App-header">
-        <div className=''>
+        
           
-        </div>
+        
       </header>
     </div>
   );
